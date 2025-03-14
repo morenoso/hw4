bd6f2dce7f5cb5478eb0d8a21e2f0b8f6da1afaf
Luis Moreno Sorto <morenoso@usc.edu>
2025-03-14 11:40:19 -0700

    equal-paths done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,35 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int pathHeight(Node* currNode, int currHeight);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    //its either empty or is the leaf
+    if(root == nullptr ) {
+        return true;
+    }
+    //check if the heights of the paths are the same
+    //or if there is only one leaf node
+    if(pathHeight(root->left, 0) == pathHeight(root->right, 0) || (pathHeight(root->left,0) == 0 && pathHeight(root->right,0)==1)
+        || (pathHeight(root->left, 0) == 1 && pathHeight(root->right,0) == 0)) {
+        return true;
+    }
+    return false;
 
 }
 
+int pathHeight(Node* currNode, int currHeight) {
+    if(currNode == nullptr) {
+        return currHeight;
+    }
+    int lHeight = pathHeight(currNode->left, currHeight+1);
+    int rHeight = pathHeight(currNode->right, currHeight +1);
+
+    if(lHeight > rHeight) {
+        return lHeight;
+    }else {
+        return rHeight;
+    }
+}
